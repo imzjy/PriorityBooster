3e9249a90fba0e91669847f9916371274c45fddc
Jerry Chou <zjy.dev@gmail.com>
2023-04-17 15:01:27 +0800

    implement major dispatch function which set thread's priority

---
+++ PriorityBooster/PriorityBoosterCommon.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <ntddk.h>
+
+#define PRIORITY_BOOSTER_DEVICE 0x8000
+
+#define IOCTL_PRIORITY_BOOSTER_SET_PRIORITY CTL_CODE(PRIORITY_BOOSTER_DEVICE, \
+	0x800, METHOD_NEITHER, FILE_ANY_ACCESS)
+
+UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\PriorityBooster");
+
+struct ThreadData
+{
+	ULONG ThreadId;
+	int Priority;
+};
