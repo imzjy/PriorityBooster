771cf9400f35b445e1d627ba89a778270f7b92a4
Jerry Chou <zjy.dev@gmail.com>
2023-04-17 14:08:38 +0800

    add CreateFile and CloseFile support, so that client can open and close handle to the driver

--- PriorityBooster/PriorityBooster.cpp
+++ PriorityBooster/PriorityBooster.cpp
@@ -1,10 +1,30 @@
 #include <ntddk.h>
 
+void PriorityBoosterUnload(_In_ PDRIVER_OBJECT DriverObject);
+
+NTSTATUS PriorityBoosterCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp);
+
+
 extern "C" NTSTATUS
 DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
 	UNREFERENCED_PARAMETER(DriverObject);
 	UNREFERENCED_PARAMETER(RegistryPath);
 
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = PriorityBoosterCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_CLOSE] = PriorityBoosterCreateClose;
+
 
 	return STATUS_SUCCESS;
+}
+
+void PriorityBoosterUnload(_In_ PDRIVER_OBJECT DriverObject) {
+	UNREFERENCED_PARAMETER(DriverObject);
+}
+
+NTSTATUS PriorityBoosterCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp) {
+	UNREFERENCED_PARAMETER(DeviceObject);
+	UNREFERENCED_PARAMETER(Irp);
+
+	return STATUS_SUCCESS;
+
 }
